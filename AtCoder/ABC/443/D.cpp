e4816f785f1a471880c1d23ddc43bbcec1b23f70
wcg316 <henry.rem.rem@gmail.com>
2026-09-07 23:55:20 +0800

    solving 443D

---
+++ AtCoder/ABC/443/D.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+//#define int long long
+//#define getchar getchar_unlocked
+//#define putchar putchar_unlocked
+using namespace std;
+
+inline int read() {
+    char ch = getchar();
+    while (ch == ' ' || ch == '\n') ch = getchar();
+    if (ch == EOF) return EOF;
+    int s = 1;
+    while (ch < '0' || '9' < ch) {
+        if (ch == '-') s = -1;
+        ch = getchar();
+    }
+    int r = 0;
+    while ('0' <= ch && ch <= '9') r = r * 10 + ch - '0', ch = getchar();
+    return r * s;
+}
+
+void write(int x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) write(x / 10);
+    putchar(x % 10 + '0');
+}
+
+signed main() {
+    int t = read();
+	while (t--) {
+		int n = read();
+		int ans = 0;
+		vector<int> rows(n);
+		for (auto& row : rows) {
+			row = read();
+		}
+		
+		write(ans);
+		putchar('\n');
+	}
+	return 0;
+}
