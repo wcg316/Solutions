7e2144eb886e841d987c0fa7f6a437008668d278
wcg316 <henry.rem.rem@gmail.com>
2026-09-23 17:38:56 +0800

    solved 072D

---
+++ AtCoder/ABC/072/D.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+//#define int long long
+//#define getchar getchar_unlocked
+//#define putchar putchar_unlocked
+using namespace std;
+
+inline int read() {
+    char ch = getchar();
+    while (ch == ' ' || ch == '\n') ch = getchar();
+    if (ch == EOF) return EOF;
+    int s = 1;
+    while (ch < '0' || '9' < ch) {
+        if (ch == '-') s = -1;
+        ch = getchar();
+    }
+    int r = 0;
+    while ('0' <= ch && ch <= '9') r = r * 10 + ch - '0', ch = getchar();
+    return r * s;
+}
+
+void write(int x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) write(x / 10);
+    putchar(x % 10 + '0');
+}
+
+signed main() {
+    int n = read();
+	vector<int> vec(n);
+	for (auto& v : vec) {
+		v = read();
+	}
+	int ans = 0;
+	for (int i = 0; i < n; i++) {
+		if (vec[i] == i + 1) {
+			if (vec[i + 1] == i + 2) {
+				i++;
+			}
+			ans++;
+		}
+	}
+	write(ans);
+    return 0;
+}
