b4a2bef962f0e19720a172f3f2a5121d2fa27a20
wcg316 <henry.rem.rem@gmail.com>
2026-09-25 22:35:59 +0800

    solved 082C

---
+++ AtCoder/ABC/082/C.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <unordered_map>
+//#define int long long
+//#define getchar getchar_unlocked
+//#define putchar putchar_unlocked
+using namespace std;
+
+inline int read() {
+    char ch = getchar();
+    while (ch == ' ' || ch == '\n') ch = getchar();
+    if (ch == EOF) return EOF;
+    int s = 1;
+    while (ch < '0' || '9' < ch) {
+        if (ch == '-') s = -1;
+        ch = getchar();
+    }
+    int r = 0;
+    while ('0' <= ch && ch <= '9') r = r * 10 + ch - '0', ch = getchar();
+    return r * s;
+}
+
+void write(int x) {
+    if (x < 0) {
+        putchar('-');
+        x = -x;
+    }
+    if (x > 9) write(x / 10);
+    putchar(x % 10 + '0');
+}
+
+signed main() {
+    int n = read();
+	unordered_map<int, int> um;
+    while (n--) {
+        um[read()]++;
+    }
+    int ans = 0;
+    for (auto& num : um) {
+        ans += num.second < num.first ? num.second : num.second - num.first;
+    }
+    write(ans);
+	return 0;
+}
